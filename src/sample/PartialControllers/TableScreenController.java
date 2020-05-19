cc13d2bff87802964c21ce99b1dc42d60bc40249
Rinor <rinor.ahmeti@studentet.uni-pr.edu>
2020-05-19 14:51:27 +0200

    "SO MUCH WORK TO BE DONE :@:@:@"

--- src/sample/PartialControllers/TableScreenController.java
+++ src/sample/PartialControllers/TableScreenController.java
@@ -1,5 +1,9 @@
 package sample.PartialControllers;
 
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+import sample.kamarieri.Controller;
 import StateClasses.Tables;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,7 +13,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import sample.Menu.DbMenu;
@@ -21,9 +24,6 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.geometry.Insets;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import sample.kamarieri.Controller;
 import StateClasses.Dbinfo;
 
@@ -44,6 +44,16 @@ public class TableScreenController implements Initializable {
     private Label tableCapacity;
     @FXML
     private ComboBox menuCombo;
+    @FXML
+    private TableView detailsTable;
+    @FXML
+    private TableColumn productColumn;
+    @FXML
+    private TableColumn quantityColumn;
+    @FXML
+    private TableColumn priceColumn;
+    @FXML
+    private Text orderText;
 
     public void setTable(Tables table) throws SQLException {
         this.table = table;
@@ -65,6 +75,40 @@ public class TableScreenController implements Initializable {
 
     }
 
+    @FXML
+    public void openDetails(ActionEvent actionEvent)
+    {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../partials/Details.fxml"));
+        } catch (IOException e) {
+            System.out.println("Path eshte gabim");
+        }
+        Scene dashboard = new Scene(root);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(dashboard);
+        window.show();
+
+
+    }
+    public double getPrice() throws Exception {
+        Connection con = Dbinfo.startConnection();
+        String sql = "Select price from Menu where id=?";
+        PreparedStatement stmt = con.prepareStatement(sql);
+
+        ResultSet rez = stmt.executeQuery();
+        double price;
+        if (rez.next())
+        {
+            price = rez.getDouble(1);
+            return price;
+        }
+        else
+        {
+            throw new Exception("Gabim!");
+        }
+
+    }
 
     public void getMenuItems() throws SQLException {
         ArrayList<String> items = new ArrayList<>();
@@ -121,6 +165,7 @@ public class TableScreenController implements Initializable {
         this.table.increaseTotalPrice(price*quantity);
         System.out.println(this.table.toString());
 
+        con.close();
     }
 
     @Override
